5204cb421aca2fb24b785b08374b9916bbec1558
Anastasiia Solomchuk <ssi_asolomchuk@qbsol.com>
2022-01-13 17:36:13 +0200

    fix

--- modules/compute/virtual_machine_extensions/Microsoft.EnterpriseCloud.Monitoring.tf
+++ modules/compute/virtual_machine_extensions/Microsoft.EnterpriseCloud.Monitoring.tf
@@ -16,7 +16,7 @@ resource "azurerm_virtual_machine_extension" "monitoring" {
   )
   protected_settings = jsonencode(
     {
-      "workspaceKey" : data.external.monitoring_test_workspace_key["enabled"].result.primarySharedKey
+      "workspaceKey" : data.external.monitoring_TEST_workspace_key["enabled"].result.primarySharedKey
     }
   )
 
@@ -26,22 +26,22 @@ resource "azurerm_virtual_machine_extension" "monitoring" {
 data "external" "monitoring_test_workspace_key" {
   for_each = var.extension_name == "microsoft_enterprise_cloud_monitoring" ? toset(["enabled"]) : toset([])
 
-  program = can(var.settings.diagnostics.log_analytics[var.extension.diagnostic_log_analytics_key].name) != false ?  [
-    "bash", "-c",
-    format(
-      "az monitor log-analytics workspace get-shared-keys --workspace-name '%s' --resource-group '%s' --subscription '%s' --query '{primarySharedKey: primarySharedKey }' -o json",
-      var.diagnostics.diagnostics_destinations.log_analytics[var.extension.diagnostic_log_analytics_key].log_analytics_name,
-      var.diagnostics.diagnostics_destinations.log_analytics[var.extension.diagnostic_log_analytics_key].log_analytics_resource_group_name,
-      var.diagnostics.diagnostics_destinations.log_analytics[var.extension.diagnostic_log_analytics_key].log_analytics_sub
-    )
-  ]: [
-    "bash", "-c",
+  program = can(var.settings.diagnostics.log_analytics[var.extension.diagnostic_log_analytics_key].name) != false ? [
+   "bash", "-c",
     format(
       "az monitor log-analytics workspace get-shared-keys --workspace-name '%s' --resource-group '%s' --subscription '%s' --query '{primarySharedKey: primarySharedKey }' -o json",
       var.settings.diagnostics.log_analytics[var.extension.diagnostic_log_analytics_key].name,
       var.settings.diagnostics.log_analytics[var.extension.diagnostic_log_analytics_key].resource_group_name,
       substr(var.settings.diagnostics.log_analytics[var.extension.diagnostic_log_analytics_key].id, 15, 36)
     )
-  ] 
+  ] :  [
+    "bash", "-c",
+    format(
+      "az monitor log-analytics workspace get-shared-keys --workspace-name '%s' --resource-group '%s' --subscription '%s' --query '{primarySharedKey: primarySharedKey }' -o json",
+      var.settings.diagnostics.diagnostics_destinations.log_analytics[var.extension.diagnostic_log_analytics_key].log_analytics_name,
+      var.settings.diagnostics.diagnostics_destinations.log_analytics[var.extension.diagnostic_log_analytics_key].log_analytics_resource_group_name,
+      var.settings.diagnostics.diagnostics_destinations.log_analytics[var.extension.diagnostic_log_analytics_key].log_analytics_sub
+    )
+  ]
 }
 
