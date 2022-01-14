5a5b5bbaa43c772a73d808eb90f62536c8029de7
Anastasiia Solomchuk <ssi_asolomchuk@qbsol.com>
2022-01-14 16:45:21 +0200

    fix

--- modules/compute/virtual_machine_extensions/Microsoft.EnterpriseCloud.Monitoring.tf
+++ modules/compute/virtual_machine_extensions/Microsoft.EnterpriseCloud.Monitoring.tf
@@ -26,7 +26,14 @@ resource "azurerm_virtual_machine_extension" "monitoring" {
 data "external" "monitoring_workspace_key" {
   for_each = var.extension_name == "microsoft_enterprise_cloud_monitoring" ? toset(["enabled"]) : toset([])
 
-  program = can(var.settings.diagnostics.log_analytics[var.extension.diagnostic_log_analytics_key].name) != false ? [
+  program =  [
+    "bash", "-c",
+    format(
+      "az monitor log-analytics workspace get-shared-keys --workspace-name 'qs-log-caf-laworkspace-tvt' --resource-group 'qs-rg-caf-monitor-lqr' --subscription 'f74984f0-0da0-43b7-a4d9-bb1146800950' --query '{primarySharedKey: primarySharedKey }' -o json"
+    )
+  ]
+  
+  /*can(var.settings.diagnostics.log_analytics[var.extension.diagnostic_log_analytics_key].name) != false ? [
    "bash", "-c",
     format(
       "az monitor log-analytics workspace get-shared-keys --workspace-name '%s' --resource-group '%s' --subscription '%s' --query '{primarySharedKey: primarySharedKey }' -o json",
@@ -42,6 +49,6 @@ data "external" "monitoring_workspace_key" {
       var.settings.diagnostics.diagnostics_destinations.log_analytics[var.extension.diagnostic_log_analytics_key].log_analytics_resource_group_name,
       var.settings.diagnostics.diagnostics_destinations.log_analytics[var.extension.diagnostic_log_analytics_key].log_analytics_sub
     )
-  ]
+  ]*/
 }
 
